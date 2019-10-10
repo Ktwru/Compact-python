bf97356bca71d59e51bda9299d277bf2eb165562
Ktwru <spacewarlushd@gmail.com>
2019-10-10 21:52:58 +0300

    better spiral-matrix function

--- Spiral.py
+++ Spiral.py
@@ -20,58 +20,34 @@ def spiral(lenght):
     ops = matrix_side//2
     side = 2
 
-    y+=1
-    for op in range(ops):
-        x-=1
-        y-=1
+
+    for op in range(ops):                                                       #spiral matrix
+        x -= 1
+        y -= 1
         for leftside in range(side):
             num+=1
-            matrix[y+leftside][x]=num
+            y+=1
+            matrix[y][x]=num
         for downside in range(side):
             num+=1
-            matrix[y+side-1][x+downside+1]=num
+            x+=1
+            matrix[y][x]=num
         for rightside in range(side):
             num+=1
-            matrix[y+side-2-rightside][x+side]=num
+            y-=1
+            matrix[y][x]=num
         for upside in range(side):
             num+=1
-            matrix[y-1][x+side-1-upside]=num
+            x-=1
+            matrix[y][x]=num
         side+=2
 
     print('matrix side:', matrix_side, 'x:', x, "y:", y, 'ops:', ops)
 
-    # matrix = [[0 for j in range(n)] for i in range(n)]
-    #
-    # ops = (n//2)
-    # num = 0
-    # s = n - 1
-    # x = 0
-    # y = 0
-    # for i in range(ops):
-    #     for upside in range(s):
-    #         num += 1
-    #         matrix[y][x + upside] = num
-    #     for rightside in range(s):
-    #         num += 1
-    #         matrix[y + rightside][x + s] = num
-    #     for downside in range(s):
-    #         num += 1
-    #         matrix[y + s][x + s - downside] = num
-    #     for leftside in range(s):
-    #         num += 1
-    #         matrix[y + s - leftside][x] = num
-    #     x+=1
-    #     y+=1
-    #     s-=2
-    #
-    # if n%2:
-    #     matrix[n//2][n//2]=n*n
 
-    #for i in matrix:
-    #    print(i)
     for i in matrix:
         print(i)
 
 
-print(spiral(10))
+print(spiral(13))
 
