c02be2ec9719b9b2ca39359ca0cf7c7130ee9750
Ktwru <spacewarlushd@gmail.com>
2019-10-14 23:24:58 +0300

    xray

--- Main.py
+++ Main.py
@@ -49,24 +49,33 @@ def main():
     window = pygame.display.set_mode((850, 600))
     window.fill((202, 151, 39))
     folded_snake = pygame.Surface((500, 500))
-    folded_snake.fill((0, 255, 0))
+    folded_snake.fill((202, 151, 39))
     text = ['Python length:', 'fold', 'X-ray']
     font = pygame.font.Font('PixelFont.ttf', 36)
-    t_p_l = font.render(text[0], 0, (0, 0, 0))
-    t_f = font.render(text[1], 0, (0, 0, 0))
-    t_x = font.render(text[2], 0, (0, 0, 0))
     lenghtX = 260
     python_lenght = 1
+    xray = 0
 
     pygame.display.update()
     while True:
+        buttondown = 0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                buttondown = 1
         pygame.time.delay(20)
-        window.fill((202, 151, 39))
+        if xray == 0:
+            colors = [(202, 151, 39), (0, 0, 0), (255, 255, 255)]
+        else:
+            colors = [(0, 0, 0), (255, 255, 255), (0, 0, 0)]
+        window.fill(colors[0])
         pos = pygame.mouse.get_pos()
         mos = pygame.mouse.get_pressed()[0]
+        t_p_l = font.render(text[0], 0, colors[1])
         window.blit(t_p_l, (15, 30))
-        lenght = pygame.draw.rect(window, (0, 0, 0), (270, 43, 450, 5))
-        pygame.draw.rect(window, (255, 255, 255), (lenghtX-10, 33, 20, 20))
+        lenght = pygame.draw.rect(window, colors[1], (270, 43, 450, 5))
+        pygame.draw.rect(window, colors[1], (lenghtX-10, 33, 20, 20))
         if pos[0] >= lenght.left and pos[0] <= lenght.left + lenght.width and pos[1] >= 5 and pos[1] <= 70:
             if mos:
                 lenghtX = pos[0]
@@ -74,17 +83,17 @@ def main():
                 python_lenght = lenghtX - lenght.left
                 if python_lenght == 0:
                     python_lenght = 1
-        pygame.draw.rect(window, (255, 255, 255), (740, 21, 85, 50))
-        pygame.draw.rect(window, (0, 0, 0), (740, 21, 85, 50), 5)
-        num_p_l = font.render(str(python_lenght), 0, (0, 0, 0))
+        pygame.draw.rect(window, colors[2], (740, 21, 85, 50))
+        pygame.draw.rect(window, colors[1], (740, 21, 85, 50), 5)
+        num_p_l = font.render(str(python_lenght), 0, colors[1])
         window.blit(num_p_l, (760, 30))
 
 
-        pygame.draw.rect(window, (255, 255, 255), (700, 100, 125, 50))
-        pygame.draw.rect(window, (0, 0, 0), (700, 100, 125, 50), 5)
+        pygame.draw.rect(window, colors[2], (700, 100, 125, 50))
+        pygame.draw.rect(window, colors[1], (700, 100, 125, 50), 5)
         if pos[0] >= 700 and pos[0] <= 825 and pos[1] >= 100 and pos[1] <= 150:
             pygame.draw.rect(window, (0, 200, 0), (700, 100, 125, 50), 5)
-            if mos:
+            if buttondown:
                 mat = spiral.spiral(python_lenght)
                 matrix_side = mat[1]
                 matrix = mat[0]
@@ -95,25 +104,34 @@ def main():
                 else:
                     coef = 2
                 part_font_size = coef * (500//matrix_side)//len(str(python_lenght))
-                folded_snake.fill((0, 255, 0))
+                folded_snake.fill(colors[0])
                 part_font = pygame.font.Font('PixelFont.ttf', part_font_size)
                 for y in range(len(matrix)):
                     for x in matrix[y]:
-                        folded_snake.blit(part_font.render((str(x)), 0, (0, 0, 0)), (part_cords[0], part_cords[1]))
+                        folded_snake.blit(part_font.render((str(x)), 0, colors[1]), (part_cords[0], part_cords[1]))
                         part_cords[0] += part_size
                     part_cords[1] += part_size
                     part_cords[0] = 0
+        t_f = font.render(text[1], 0, colors[1])
         window.blit(t_f, (730, 110))
-        pygame.draw.rect(window, (255, 255, 255), (700, 200, 125, 50))
-        pygame.draw.rect(window, (0, 0, 0), (700, 200, 125, 50), 5)
-        if pos[0] >= 700 and pos[0] <= 825 and pos[1] >= 200 and pos[1] <= 250:
+        pygame.draw.rect(window, colors[2], (700, 200, 125, 50))
+        if xray == 0:
+            pygame.draw.rect(window, colors[1], (700, 200, 125, 50), 5)
+        else:
             pygame.draw.rect(window, (0, 200, 0), (700, 200, 125, 50), 5)
+        if pos[0] >= 700 and pos[0] <= 825 and pos[1] >= 200 and pos[1] <= 250:
+            if buttondown and xray == 0:
+                xray = 1
+            elif buttondown and xray == 1:
+                xray = 0
+            pygame.draw.rect(window, (0, 200, 0), (700, 200, 125, 50), 5)
+        t_x = font.render(text[2], 0, colors[1])
         window.blit(t_x, (720, 210))
 
+        jjj = font.render(str(xray), 0, colors[1])
+        window.blit(jjj, (720, 500))
         window.blit(folded_snake, (50, 80))
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
+
         pygame.display.update()
 
 if __name__ == "__main__":
